b203d5cacc7674bae0b897a820fb509a9e792d8a
CesarCarias <cesarleonardocariasvasquez@gmail.com>
2024-02-26 10:23:33 -0400

    Sign Up added

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps Locales
     'blogs.apps.BlogsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
